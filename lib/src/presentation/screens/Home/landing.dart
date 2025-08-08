3880a860d82304e5ccef274246bfcbfd01d48c20
albinantony <albinantony2420@gmail.com>
2025-08-08 13:28:45 +0530

    changes updated in landing page

--- lib/src/presentation/screens/Home/landing.dart
+++ lib/src/presentation/screens/Home/landing.dart
@@ -56,167 +56,201 @@ class Landingscreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: height * 0.1),
-          Center(
-            child: Text(
-              "Do you require\nMedical assistance?",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                fontWeight: FontWeight.w700,
-                fontSize: width * 0.07,
-                color: const Color(0xff353459),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: height * 0.1),
+            Center(
+              child: Text(
+                "Do you require\nMedical assistance?",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w700,
+                  fontSize: width * 0.07,
+                  color: const Color(0xff353459),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: height * 0.04),
-          Text(
-            "Tap the button to request an ambulance.",
-            style: GoogleFonts.poppins(
-              fontWeight: FontWeight.w300,
-              fontSize: width * 0.04,
-              color: const Color(0xff747474),
+            SizedBox(height: height * 0.04),
+            Text(
+              "Tap the button to request an ambulance.",
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.w300,
+                fontSize: width * 0.04,
+                color: const Color(0xff747474),
+              ),
             ),
-          ),
-          SizedBox(height: height * 0.05),
-          GestureDetector(
-            onTap: () {
-              if (controller.lat.value != 0.0 && controller.long.value != 0.0) {
-                if (!EasyLoading.isShow) {
-                  controller.registerEmergency();
+            SizedBox(height: height * 0.02),
+            GestureDetector(
+              onTap: () {
+                if (controller.lat.value != 0.0 &&
+                    controller.long.value != 0.0) {
+                  if (!EasyLoading.isShow) {
+                    controller.registerEmergency();
+                  }
+                } else {
+                  controller.startListeningToLocation();
                 }
-              } else {
-                controller.startListeningToLocation();
-              }
-            },
-            child: SizedBox(
-              height: width * 0.7,
-              width: width * 0.7,
-              child: Image.asset("assets/icons/Frame 109.png"),
+              },
+              child: Center(
+                child: Image.asset(scale: 5, "assets/icons/Frame 109.png"),
+              ),
             ),
-          ),
-        ],
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: height * 0.02),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: width * 0.06,
+                        width: width * 0.06,
+                        child: Image.asset("assets/icons/destination.png"),
+                      ),
+                      SizedBox(width: width * 0.01),
+                      Text(
+                        "Current Location",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w700,
+                          fontSize: width * 0.05,
+                          color: Color(0xff28274F),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Obx(() {
+                  return controller.location.value != "initial"
+                      ? Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            controller.location.value,
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff28274F),
+                              fontSize: width * 0.045,
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Please turn on location services to continue.",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff28274F),
+                              fontSize: width * 0.045,
+                            ),
+                          ),
+                        );
+                }),
+              ],
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: Obx(() {
-        return controller.location.value != "initial"
-            ? Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xff353459), Color(0xff27264D)],
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32),
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: height * 0.02),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: width * 0.06,
-                            width: width * 0.06,
-                            child: Image.asset("assets/icons/location.png"),
-                          ),
-                          SizedBox(width: width * 0.01),
-                          Text(
-                            "Current Location",
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w700,
-                              fontSize: width * 0.05,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff353459), Color(0xff27264D)],
+          ),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32),
+            topRight: Radius.circular(32),
+          ),
+        ),
+        child: SizedBox(
+          height: height * 0.13,
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Image.asset(
+                          scale: 4,
+                          "assets/icons/home selected.png",
+                        ),
                       ),
-                    ),
-                    SizedBox(height: height * 0.02),
-                    const Divider(color: Colors.white70, thickness: 0.5),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(
-                        width * 0.1,
-                        height * 0.025,
-                        width * 0.1,
-                        height * 0.025,
-                      ),
-                      child: Text(
-                        controller.location.value,
-                        textAlign: TextAlign.center,
+                      Text(
+                        "Home Screen",
                         style: GoogleFonts.poppins(
                           fontWeight: FontWeight.w400,
                           color: Colors.white,
-                          fontSize: width * 0.045,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            : Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xff353459), Color(0xff27264D)],
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32),
+                    ],
                   ),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: height * 0.02),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: width * 0.06,
-                            width: width * 0.06,
-                            child: Image.asset("assets/icons/Location.png"),
-                          ),
-                          SizedBox(width: width * 0.01),
-                          Text(
-                            "Current Location",
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w700,
-                              fontSize: width * 0.05,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    log("Br");
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Image.asset(
+                          scale: 4,
+                          "assets/icons/requestBlood unselected.png",
+                        ),
                       ),
-                    ),
-                    SizedBox(height: height * 0.02),
-                    const Divider(color: Colors.white70, thickness: 0.5),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(
-                        width * 0.1,
-                        height * 0.025,
-                        width * 0.1,
-                        height * 0.025,
-                      ),
-                      child: Text(
-                        "Please turn on location services to continue.",
-                        textAlign: TextAlign.center,
+                      Text(
+                        "Request Blood",
                         style: GoogleFonts.poppins(
                           fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                          fontSize: width * 0.045,
+                          color: Color(0xff9493A7),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              );
-      }),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    log("Br");
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Image.asset(
+                          scale: 4,
+                          "assets/icons/donateBlood unselected.png",
+                        ),
+                      ),
+                      Text(
+                        "Request Blood",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff9493A7),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
